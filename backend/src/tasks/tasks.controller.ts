cd2b070e836e8facdc855014c27242fc89a50c0e
sureshkannan5 <sureshkannan082@gmail.com>
2024-03-08 00:02:05 +0530

    Fear: backend crud operations,validations

--- backend/src/tasks/tasks.controller.ts
+++ backend/src/tasks/tasks.controller.ts
@@ -1,7 +1,19 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Query,
+  Post,
+  Get,
+  Param,
+  NotFoundException,
+  BadRequestException,
+  Put,
+  Delete,
+} from '@nestjs/common';
 import { BASE_URL } from 'src/utils/constants';
 import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/createTasks.dto';
+import { UpdateTasksDto } from './dto/updateTasks.dto';
 
 @Controller(`${BASE_URL}/task`)
 export class TasksController {
@@ -9,6 +21,52 @@ export class TasksController {
 
   @Post()
   async createTasks(@Body() createTaskDto: CreateTaskDto) {
-    return this.taskServices.createTask(createTaskDto);
+    try {
+      return this.taskServices.createTask(createTaskDto);
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
+  }
+  @Get()
+  async listAllTasks(
+    @Query('pageNumber') pageNumber: number = 1,
+    @Query('limit') limit: number = 10,
+  ) {
+    try {
+      return this.taskServices.listAllTasks(pageNumber, limit);
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
+  }
+
+  @Get(':id')
+  async getTaskById(@Param('id') id: string) {
+    try {
+      return this.taskServices.getTaskById(id);
+    } catch (error) {
+      console.log(error);
+      throw new NotFoundException(error.message);
+    }
+  }
+  @Put(':id')
+  async updateTaskById(
+    @Param('id') id: string,
+    @Body() updateTaskDto: UpdateTasksDto,
+  ) {
+    try {
+      return this.taskServices.updateTaskById(id, updateTaskDto);
+    } catch (error) {
+      console.log(error);
+      throw new NotFoundException(error.message);
+    }
+  }
+  @Delete(':id')
+  async deleteTaskById(@Param('id') id: string) {
+    try {
+      return this.taskServices.deleteTaskById(id);
+    } catch (error) {
+      console.log(error);
+      throw new NotFoundException(error.message);
+    }
   }
 }
--- backend/src/tasks/tasks.service.ts
+++ backend/src/tasks/tasks.service.ts
@@ -1,23 +1,104 @@
-import { Inject, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Inject,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { TASK_MODEL } from 'src/utils/constants';
 import { CreateTaskDto } from './dto/createTasks.dto';
 import { Model } from 'mongoose';
 import { Tasks } from './interfaces/task.interfaces';
+import { UpdateTasksDto } from './dto/updateTasks.dto';
 
 @Injectable()
 export class TasksService {
   constructor(@Inject(TASK_MODEL) private userModal: Model<Tasks>) {}
 
-  async createTask(createTaskDto: CreateTaskDto) {
-    console.log('coming into task', createTaskDto);
+  // create task
+  async createTask(createTaskDto: CreateTaskDto): Promise<Tasks> {
+    try {
+      // check db connection
+      if (this.checkDatabaseConnection()) {
+        console.log('DB is connected Sucessfully');
+        // save the take dto to db
+        const taskInstance = new this.userModal(createTaskDto);
+        return await taskInstance.save();
+      }
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
+  }
 
-    if (this.checkDatabaseConnection()) {
-      console.log('DB is connected Sucessfully');
+  // list all tasks with pagination
+
+  async listAllTasks(page: number = 1, limit: number = 10): Promise<Tasks[]> {
+    try {
+      if (this.checkDatabaseConnection()) {
+        // skip the records for paginated results
+        const skip = (page - 1) * limit;
+
+        return this.userModal.find().skip(skip).limit(limit).exec();
+      }
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
+  }
+
+  // get task by ID
+
+  async getTaskById(id: string): Promise<Tasks> {
+    try {
+      if (this.checkDatabaseConnection()) {
+        const task = await this.userModal.findById(id);
+        if (!task) {
+          throw new NotFoundException('Task not found');
+        }
+        return task;
+      }
+    } catch (error) {
+      throw new NotFoundException('Task not found');
+    }
+  }
+
+  async updateTaskById(
+    id: string,
+    updateTaskDto: UpdateTasksDto,
+  ): Promise<Tasks> {
+    try {
+      if (this.checkDatabaseConnection()) {
+        const existingTask = await this.userModal.findById(id);
+        // check whether task existing or not
+        if (!existingTask) {
+          throw new NotFoundException('Task not found');
+        }
+        // update the field from client
+        Object.assign(existingTask, updateTaskDto);
+
+        return await existingTask.save();
+      }
+    } catch (error) {
+      throw new NotFoundException('Task not found');
+    }
+  }
+
+  async deleteTaskById(id: string) {
+    try {
+      if (this.checkDatabaseConnection()) {
+        const task = await this.userModal.deleteOne({ _id: id });
+        if (!task) {
+          throw new NotFoundException('Task not found');
+        }
+        console.log('task', task);
+        return { message: 'Task deleted sucessfully' };
+      }
+    } catch (error) {
+      throw new NotFoundException('Task not found');
     }
   }
 
   async checkDatabaseConnection(): Promise<boolean> {
-    const isConnected = this.userModal.db.readyState === 1; // 1 means connected
+    // check DB whether it connected or not.
+    const isConnected = this.userModal.db.readyState === 1;
 
     return isConnected;
   }
